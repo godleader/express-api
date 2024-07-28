ae1251ef7b5d73140eff98a3a02573cd64b1f0ee
active117 <kenny9@gmail.com>
2024-07-28 17:16:26 +0800

    Initial commit

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
 app.use('/', routes);
+app.use('/api', routes);
+
 
 // default catch all handler
 app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not found')));
